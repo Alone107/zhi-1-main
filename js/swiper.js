799ad9d80ac0d940def55503136099b756c5d951
Alone107 <AloneOmen@yandex.ru>
2024-06-18 13:45:54 +0500

    adaptiv

--- js/swiper.js
+++ js/swiper.js
@@ -6,25 +6,6 @@ const swiper = new Swiper(".slider", {
   // }
 });
 
-// --------------------------swiper--------------------------
-
-var swiper2 = new Swiper(".mySwiper", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: 3,
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: true,
-  },
-  pagination: {
-    el: ".about-pagination",
-  },
-});
-
 // -------------------------Popup----------------------------------
 
 const button = document.querySelector(".header-zhiviesteni-btn");
